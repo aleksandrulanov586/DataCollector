011853181cc40d956b6795424579d63a582cb6ae
Alexander <serkov.gcnet@mail.ru>
2021-03-09 20:51:11 +0300

    lesson 1-9 done

--- 03_ObjectsAndClasses/test/src/main.java
+++ 03_ObjectsAndClasses/test/src/main.java
@@ -3,19 +3,20 @@ import com.skillbox.airport.Airport;
 import java.util.ArrayList;
 import java.util.List;
 
-public class main<getInstance> {
+public class main {
+
+
     public static void main(String[] args) {
 
+
+
         Airport airoport = Airport.getInstance();
-        airoport.getTerminals();
-        System.out.println("список " + airoport.getAllAircrafts());
-        List<String> list = new ArrayList<String>();
-        list.add("Boeing");
-        list.add("Airbus");
-        System.out.println("Before operation: " + list);
-        list.size();
-        int size = list.size();
-        System.out.println("Size of list = " + size);
+
+        System.out.println("список " + airoport.getAllAircrafts().size());
+        Airport airport = Airport.getInstance();
+       // int aircraftAmount = airport.getAllAircrafts().size();
+       // System.out.println(aircraftAmount);
+
 
 
     }
