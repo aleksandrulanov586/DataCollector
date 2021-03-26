ce863650083a5b1b122dd839b0dc38837862bd4b
Alexander <serkov.gcnet@mail.ru>
2021-03-26 15:34:24 +0300

    print

--- 03_ObjectsAndClasses/Basket/src/Arithmetic.java
+++ 03_ObjectsAndClasses/Basket/src/Arithmetic.java
@@ -18,16 +18,16 @@ public class Arithmetic {
     b = int2;
   }
 
-  public Arithmetic() {
-    this.a = a;
-    this.b = b;
+ public Arithmetic() {
+   this.a = a;
+   this.b = b;
   }
 
 
   public static void main(String[] args) {
 
-    Arithmetic arithmetic = new Arithmetic();
-    System.out.println(arithmetic.a + arithmetic.b);
+   // Arithmetic arithmetic = new Arithmetic();
+  //  System.out.println(arithmetic.a + arithmetic.b);
 
 
   }
