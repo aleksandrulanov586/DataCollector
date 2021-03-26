df4d3f83e2ab4802096ab62ca19d3e7bec4531a2
Alexander <serkov.gcnet@mail.ru>
2021-03-26 07:54:50 +0300

    basket

--- 03_ObjectsAndClasses/Basket/src/Arithmetic.java
+++ 03_ObjectsAndClasses/Basket/src/Arithmetic.java
@@ -11,31 +11,26 @@ public class Arithmetic {
  // public double maximumtwonumbers = 20;
  // public double minimumtwonumbers = 20;
 
+
+
   public Arithmetic(int int1, int int2) {
- //   this.sumnumbers = sumnumbers;
-//    this.differencenumbers = differencenumbers;
     a = int1;
     b = int2;
-
-
-
   }
 
+
+
   public static void main(String[] args) {
+
+    Arithmetic arithmetic = new Arithmetic();
 
 
   }
 
   public int sumnumbers;
-
+  arithmetic = a,b;
   {
-   Scanner sumnumbers = new Scanner(System.in);
-   int a, b;
-   System.out.println("Введите число А = ");
-   a = sumnumbers.nextInt();
 
-   System.out.println("Введите число В = ");
-   b = sumnumbers.nextInt();
    System.out.println("Сумма = " + (a+b));
 
   }
