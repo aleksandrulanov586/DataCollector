9e3e141c2f079b014f2c13d6d4577c4ae6560669
Alexander <serkov.gcnet@mail.ru>
2021-03-26 15:23:54 +0300

    print

--- 03_ObjectsAndClasses/Basket/src/Arithmetic.java
+++ 03_ObjectsAndClasses/Basket/src/Arithmetic.java
@@ -18,17 +18,22 @@ public class Arithmetic {
     b = int2;
   }
 
+  public Arithmetic() {
+    this.a = a;
+    this.b = b;
+  }
 
 
   public static void main(String[] args) {
 
     Arithmetic arithmetic = new Arithmetic();
+    System.out.println(arithmetic.a + arithmetic.b);
 
 
   }
 
   public int sumnumbers;
-  arithmetic = a,b;
+
   {
 
    System.out.println("Сумма = " + (a+b));
