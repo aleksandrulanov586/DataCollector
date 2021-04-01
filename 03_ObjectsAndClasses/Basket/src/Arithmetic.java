fe75dc92e9a01f7241e15e70fc40afb916fc59e6
Alexander <serkov.gcnet@mail.ru>
2021-04-01 07:08:19 +0300

    print

--- 03_ObjectsAndClasses/Basket/src/Arithmetic.java
+++ 03_ObjectsAndClasses/Basket/src/Arithmetic.java
@@ -22,20 +22,47 @@ public class Arithmetic {
    this.a = a;
    this.b = b;
   }
+  // сумму чисел
   public String info(){
     String result = "a = " + a + ", b=" + b;
     return result;
   }
+  // разность чисел
   public Integer sun(){
+    Integer i = a - b;
+    return i;
+  }
+  //произведения чисел
+  public Integer works(){
     Integer i = a + b;
     return i;
   }
+  //среднее значение этих чисел
+  public Integer averageValue(){
+    Integer i = a % b;
+    return i;
+  }
+//максимальное из двух чисел
+  public boolean max(){
+    boolean i = a > b;
+    return i;
+  }
+  // минимальное из двух чисел
+  public boolean min(){
+    boolean i = a < b;
+    return i;
+  }
+
 
 
   public static void main(String[] args) {
     Arithmetic arithmetic = new Arithmetic(1,2);
     System.out.println(arithmetic.info());
     System.out.println(arithmetic.equals(arithmetic.sun()));
+    System.out.println(arithmetic.works());
+    System.out.println(arithmetic.averageValue());
+    System.out.println(arithmetic.max());
+    System.out.println(arithmetic.min());
 
    // Arithmetic arithmetic = new Arithmetic();
   //  System.out.println(arithmetic.a + arithmetic.b);
