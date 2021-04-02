7992bed6a37e8fd631a1a67bac8931c4fad83aa1
Alexander <serkov.gcnet@mail.ru>
2021-04-02 18:42:21 +0300

    bas

--- 03_ObjectsAndClasses/Basket/src/Printer.java
+++ 03_ObjectsAndClasses/Basket/src/Printer.java
@@ -1,19 +1,43 @@
 public class Printer
 {
+
+
   //список документов
 public  String stringQueue = "";
+// общее количество страниц но ещё не распечатанных
+public String pages;
+// переменная которая хранит не распечатанные количество страниц
+public int pagesInt = 0;
+// общее количество документов но ещё не распечатаны
+public String documents;
+//переменная, в которой  храниться не распечатаны документы
+public int documentsint = 0 ;
+
+public int  documentPages = 0;
 
 // свойства документов
   public  String documentText;
   public  String name;
 public  int numberOfPages = 0;
+  private int limit;
 
-
-
-  public static void main(String[] args)
-  {
-
+  public Printer () {
+    increaseCount(1);
+    stringQueue = "Список страниц:";
+    this.limit = 1000;
   }
+  public Printer (int limit) {
+    this();
+    this.limit = limit;
+  }
+
+  public Printer (String items, int totalPrice) {
+    this();
+    this.pages = this.pages + pages;
+    this.pagesInt = pagesInt ;
+  }
+
+
 // принятие на печать 2 метода
   public  void accepForPrinting1(String stringQueue, String name, String documentText)
   {
@@ -32,7 +56,7 @@ public  int numberOfPages = 0;
  }
 
   // метод выводящий всю информацию о добавленных в принтер документах в консоль и очищающий очередь печати после этого.
- public String print(String title)
+ public void print(String title)
 
   {
     System.out.println(title);
@@ -52,15 +76,15 @@ return  stringQueue;
 
   }
   // возвращающий общее количество документов, которые добавлены в принтер, но ещё не распечатаны, а также соответствующую переменную, в которой будет храниться это количество.
-//  public static int getDocumentsCount()
+ public  String getDocumentsCount()
 
   {
-
+return documents;
   }
   // возвращающий общее количество распечатанных документов и страниц
-//  public static int returnOfDocumentsPages()
+ public  int returnOfDocumentsPages()
 
   {
-
+return documentPages;
   }
 }
