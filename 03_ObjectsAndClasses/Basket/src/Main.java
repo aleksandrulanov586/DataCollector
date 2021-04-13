c753dcaca4fcc43c1e6091b118f05a3eb8f2eb68
Alexander <serkov.gcnet@mail.ru>
2021-04-13 12:47:42 +0300

    bas

--- 03_ObjectsAndClasses/Basket/src/Main.java
+++ 03_ObjectsAndClasses/Basket/src/Main.java
@@ -1,25 +1,24 @@
 public class Main {
-    public static void main(String[] args) {
+
+  public static void main(String[] args) {
 //==================================================================================================
-  //      Arithmetic arithmetic = new Arithmetic(1,2);
-  //      System.out.println(arithmetic.info());
+    //      Arithmetic arithmetic = new Arithmetic(1,2);
+    //      System.out.println(arithmetic.info());
 //==================================================================================================
 
-Basket basket = new Basket();
-      basket.add("Кефир", 60);
-     basket.add("Бетон", 100, 2);
-     basket.add("Бетон", 100, 2, 200.0);
-int a = basket.getTotalPrice() / basket.items;
- System.out.println(basket.getTotalPrice());
+    Basket basket = new Basket();
+    basket.add("Кефир", 60);
+    basket.add("Бетон", 100, 2);
+    basket.add("Бетон", 100, 2, 200.0);
+    int a = basket.getTotalPrice() / basket.items;
+    System.out.println(basket.getTotalPrice());
 //==================================================================================================
-   //  Printer printer = new Printer();
+    //  Printer printer = new Printer();
 
-   //   printer.append("Text");
-   //  printer.append("Text",1,"Text");
-   //   printer.append("Text","Text");
-   //   printer.print("Распечатаны документы:");
+    //   printer.append("Text");
+    //  printer.append("Text",1,"Text");
+    //   printer.append("Text","Text");
+    //   printer.print("Распечатаны документы:");
 
-
-
-    }
+  }
 }
