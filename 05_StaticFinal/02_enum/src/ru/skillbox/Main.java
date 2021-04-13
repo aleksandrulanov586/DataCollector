8403ec870901e053a20bce029a273a54af6287bb
Alexander <serkov.gcnet@mail.ru>
2021-04-13 17:33:12 +0300

    lesson 1-9 done

--- 05_StaticFinal/02_enum/src/ru/skillbox/Main.java
+++ 05_StaticFinal/02_enum/src/ru/skillbox/Main.java
@@ -3,5 +3,10 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
+
+        ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator (2,2);
+        arithmeticCalculator.getA();
+
+        System.out.println(Operation.ADD);
     }
 }
