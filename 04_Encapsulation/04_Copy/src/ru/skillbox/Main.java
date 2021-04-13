75015d5a5f8ab6be900aa020bab347fe01c252ff
Alexander <serkov.gcnet@mail.ru>
2021-04-13 11:18:57 +0300

    cop

--- 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
@@ -5,19 +5,19 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
-        Dimensions dimensions = new Dimensions();
+        Dimensions dimensions = new Dimensions(" Tekst ", " можно переворачивать", " 154154", " Tekst", 100, 200.50);
         dimensions.setDimensions(100);
         dimensions.setDeliveryAddress("Tekst");
         dimensions.setIsTheCargoFragile("Tekst");
-        dimensions.setRegistrationNumber("152854");
-        dimensions.setWeight(200);
-        dimensions.setPropertyIsItPossibleToFlip("Tekst");
+        dimensions.setRegistrationNumber("154154");
+        dimensions.setWeight(200.50);
+        dimensions.setPropertyIsItPossibleToFlip("можно переворачивать");
         Dimensions copy = new Dimensions(dimensions.getDimensions(),
-            dimensions.getWeight(),
-            dimensions.getDeliveryAddress(),
-            dimensions.getPropertyIsItPossibleToFlip(),
-            dimensions.getRegistrationNumber(),
-            dimensions.getIsTheCargoFragile());
+                                         dimensions.getWeight(),
+                                         dimensions.getDeliveryAddress(),
+                                         dimensions.getPropertyIsItPossibleToFlip(),
+                                         dimensions.getRegistrationNumber(),
+                                         dimensions.getIsTheCargoFragile());
 
 
     }
