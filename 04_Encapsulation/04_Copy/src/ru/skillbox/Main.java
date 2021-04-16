56aca3073b115d0918d1eea241e6893bac1b3363
Alexander <serkov.gcnet@mail.ru>
2021-04-16 12:39:02 +0300

    cop

--- 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
@@ -5,20 +5,28 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
-        Dimensions copy = new Dimensions(copy.getDimensions(),
-            copy.getWeight(),
-            copy.getDeliveryAddress(),
-            copy.getPropertyIsItPossibleToFlip(),
-            copy.getRegistrationNumber(),
-            copy.getIsTheCargoFragile());
+        Dimensions copy = new Dimensions(" text",true,"text","text",10,10);
+Dimensions copycopy = new Dimensions(copy.getDeliveryAddress(), copy.getPropertyIsItPossibleToFlip(),
+    copy.getRegistrationNumber(), copy.getIsTheCargoFragile(), copy.getDimensions(),
+    copy.getWeight());
+
+System.out.println(copy);
+System.out.println(copycopy);
 
 
-        copy.setDimensions(100);
-        copy.setDeliveryAddress("Tekst");
-        copy.setIsTheCargoFragile("Tekst");
-        copy.setRegistrationNumber("154154");
-        copy.setWeight(200.50);
-        copy.setPropertyIsItPossibleToFlip(true);
+            //copy.getWeight(),
+           // copy.getDeliveryAddress(),
+           // copy.getPropertyIsItPossibleToFlip(),
+          //  copy.getRegistrationNumber(),
+          //  copy.getIsTheCargoFragile());
+
+
+       // copy.setDimensions(100);
+       // copy.setDeliveryAddress("Tekst");
+      //  copy.setIsTheCargoFragile("Tekst");
+      //  copy.setRegistrationNumber("154154");
+      //  copy.setWeight(200.50);
+      //  copy.setPropertyIsItPossibleToFlip(true);
 
 
 
