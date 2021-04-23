50c66513b9779195fc05e16aa3960b978026ce0f
Alexander <serkov.gcnet@mail.ru>
2021-04-23 06:59:50 +0300

    aaaa

--- 05_StaticFinal/03_computer/src/ru/skillbox/Main.java
+++ 05_StaticFinal/03_computer/src/ru/skillbox/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         Keyboard ansiKeyboard = new Keyboard(100.0, false, KeyboardType.ANSI);
-        Computer computer = new Computer(s);
         Storage storageHdd = new Storage(HardDiskType.HDD);
         Screen screenVa = new Screen(20,20,ScreenType.VA);
         Processor processor = new Processor(10,2000,"text",80);
+        Computer computer = new Computer(storageHdd," Text"," text",screenVa,processor,ansiKeyboard);
 
     }
 }
