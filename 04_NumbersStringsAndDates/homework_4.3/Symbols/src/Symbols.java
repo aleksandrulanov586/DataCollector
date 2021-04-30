d42d0e533482b7ee175c74a5d2579be45b3562ab
Alexander <serkov.gcnet@mail.ru>
2021-04-30 18:45:34 +0300

    lesson 1-9 done

---
+++ 04_NumbersStringsAndDates/homework_4.3/Symbols/src/Symbols.java
@@ -0,0 +1,13 @@
+public class Symbols {
+
+
+  public static void main(String[] args) {
+   // String  englishAlphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  //  System.out.println(englishAlphabet.charAt(5));
+    for (char c ='a'; c <= 'z'; c++) {
+      System.out.println("код="+ (int) c + " \t символ=" + c);
+    }
+  }
+
+}
