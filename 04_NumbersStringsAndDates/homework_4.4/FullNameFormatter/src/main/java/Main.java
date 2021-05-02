212daccb7210037a58a0e3afbca11bdd16d139a5
Alexander <serkov.gcnet@mail.ru>
2021-05-02 19:00:17 +0300

    fam

--- 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
@@ -13,6 +13,19 @@ public class Main {
 
       //TODO:напишите ваш код тут, результат вывести в консоль.
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+      String f = "Фамилия:";
+      String i = "Имя:";
+      String o = "Отчество:";
+
+      do {
+        System.out.println("Введите ФИО:");
+        System.out.println(f+ input + "\n" + i + input + "\n" + o + input );
+
+
+      }while (input.equals("ФИО"));
+     continue;
+
+
     }
   }
 
--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -6,17 +6,16 @@ public class Main {
     //TODO: напишите ваш код, результат вывести в консоль
     String textTrim = text.trim();
 
-    String text3 = text.substring(15,19);
-    String text4 = text.substring(35,39);
-    String text5 = text.substring(56,61);
+    String text3 = text.substring(15, 19);
+    String text4 = text.substring(35, 39);
+    String text5 = text.substring(56, 61);
 
-   Integer parsText3 = Integer.parseInt(text3);
+    Integer parsText3 = Integer.parseInt(text3);
     Integer parsText4 = Integer.parseInt(text4);
     Integer parsText5 = Integer.parseInt(text5);
 
     Integer sum = parsText3 + parsText4 + parsText5;
     System.out.println(sum);
-
 
 
   }
