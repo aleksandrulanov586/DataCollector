f8351336877978d3bc7ad3dfbc10dc535fc3e037
Alexander <serkov.gcnet@mail.ru>
2021-05-12 17:53:57 +0300

    q

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner user = new Scanner(System.in);
+        String string = user.nextLine();
 
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+        text = "< >";
+
+
         return text;
     }
 
--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,30 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
+   String text = "Вася заработао 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+    String p5000 = "^\\D+(\\d+).*";
+
+    Pattern vasy = Pattern.compile(p5000);
+    Matcher v = vasy.matcher(text);
+    if (v.find()) {
+      System.out.println("Found value: " + v.group(0) );
+     System.out.println("Found value: " + v.group(1) );
+     System.out.println("Found value: " + v.group(2) );
+
+    }
 
   }
 
   public static int calculateSalarySum(String text){
     //TODO: реализуйте метод
-    return -1;
+
+
+
+
+   return -1;
   }
 
 }
