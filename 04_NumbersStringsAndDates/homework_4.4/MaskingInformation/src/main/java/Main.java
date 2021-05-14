f714f1b44a4388cdad0bf4411903266d0c75ae40
Alexander <serkov.gcnet@mail.ru>
2021-05-14 11:55:40 +0300

    ф

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -2,15 +2,30 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner user = new Scanner(System.in);
-        String string = user.nextLine();
 
-    }
 
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String userInputText = scanner.nextLine();
+
+    String nonSecretText = searchAndReplaceDiamonds(userInputText, "***");
+    System.out.println(nonSecretText);
+  }
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        text = "< >";
+
+
+
+
+      int indexStartParenthesis = text.indexOf("<");
+      int indexEndParenthesis = text.lastIndexOf(">");
+
+
+
+
+
+String pruningStartParenthesis = text.substring(0,indexStartParenthesis);
+String pruningEndParenthesis = text.substring(indexEndParenthesis);
 
 
         return text;
