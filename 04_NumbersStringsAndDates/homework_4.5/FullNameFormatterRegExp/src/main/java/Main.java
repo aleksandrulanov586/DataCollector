361c1d65fe91b5840308ab6f5f550734b1fa5a02
Alexander <serkov.gcnet@mail.ru>
2021-05-19 19:03:36 +0300

    Реализация  метода вывода через консоль с помощью регулярок Фамилия: Иванов
    Имя: Сергей
    
    Отчество: Петрович

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -19,42 +19,17 @@ public class Main {
       String[] fields = p.split(input);
 
       for (int i = 0; i < fields.length; i++){
-        System.out.println(fields[i]);
+        System.out.println("Фамилия:" + fields[i]  );
+
+        System.out.println("Имя:" + fields[i] );
+
+        System.out.println("Отчество:" + fields[i]  );
+       continue;
       }
 
 
 
-     /** int spacesCount = 0;
-      for (char e : input.toCharArray()) {
-        if (e == ' ') {
-          spacesCount++;
-        }
-      }
-      if (spacesCount != 2) {
-        System.out.println("Введенная строка не является ФИО");
-        continue;
-      }
 
-      boolean hasDigit = false;
-      for (Character b = 0; b < input.length(); b++) {
-        if (Character.isDigit(input.charAt(b))) {
-          hasDigit = true;
-          break;
-
-        }
-      }
-      if (hasDigit) {
-        System.out.println("Введенная строка не является ФИО");
-        break;
-      }
-
-      String name = input.substring(0, startSpace);
-      String patronymic = input.substring(endSpace);
-      String surname = input.substring(startSpace, endSpace);
-
-      System.out.println("Фамилия: " + name.trim());
-      System.out.println("Имя: " + surname.trim());
-      System.out.println("Отчество: " + patronymic.trim()); **/
 
 
     }
