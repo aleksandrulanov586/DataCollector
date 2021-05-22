33ace8090e7f721d5227de6eaaa2dd23e7709d60
Alexander <serkov.gcnet@mail.ru>
2021-05-22 09:21:06 +0300

    lesson 1-9 done

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -15,24 +15,38 @@ public class Main {
       //TODO:напишите ваш код тут, результат вывести в консоль.
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
 
-      Pattern p = Pattern.compile("\\s+");
-      String[] fields = p.split(input);
+      Pattern.matches(".*\\p{InCyrillic}.*", input);
+      Pattern p = Pattern.compile("[А-Яа-я]+\\s+[А-Яа-я]+\\s+[А-Яа-я]+");
+      boolean isFullname = p.matcher(input).matches();
 
-      for (int i = 0; i < fields.length; i++){
-        System.out.println("Фамилия:" + fields[i]  );
-
-        System.out.println("Имя:" + fields[i] );
-
-        System.out.println("Отчество:" + fields[i]  );
-       continue;
+      int amountCyrillic = 0;
+      for (int i = 0; i < input.length(); i++) {
+        if (Character.UnicodeBlock.of(input.charAt(i)).equals(Character.UnicodeBlock.CYRILLIC)) {
+          amountCyrillic++;
+        }
+      }
+      if (amountCyrillic == 0) {
+        System.out.println(" Введенная строка не является ФИО");
+        continue;
       }
 
-
-
-
+      if (input.matches("([А-Я][а-я]+(-[А-Я][а-я]+)?\\s){2}[А-Я][а-я]+(\\s[а-я]+)?")) {
+        int spaceIndex = input.indexOf(' ');
+        int spaceIndex2 = input.lastIndexOf(' ');
+        System.out.println("Фамилия: " + (input.substring(0, spaceIndex)) + input.trim());
+        System.out.println("Имя: " + (input.substring(spaceIndex, spaceIndex2)) + input.trim());
+        System.out.println("Отчество: " + (input.substring(spaceIndex2)) + input.trim());
+        break;
+      } else {
+        System.out.println(" Вводимая строка не является ФИО ");
+        break;
+      }
 
 
     }
   }
 
+
 }
+
+
