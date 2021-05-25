0e6fd99d3c61c7381279b8174ab2f606e60bcc56
Alexander <serkov.gcnet@mail.ru>
2021-05-25 09:53:39 +0300

    Реалицация метода проверки ФИО

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -15,23 +15,15 @@ public class Main {
       //TODO:напишите ваш код тут, результат вывести в консоль.
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
 
-      //Pattern p = Pattern.compile("[А-Яа-я]+\\s+[А-Яа-я]+\\s+[А-Яа-я]+");
-     // boolean isFullname = p.matcher(input).matches();
+      Pattern p = Pattern.compile("[А-Яа-я]+\\s+[А-Яа-я]+\\s+[А-Яа-я]+");
+      boolean isFullname = p.matcher(input).matches();
 
-
-
-
-      if (input.matches("[А-Яа-я]+\\s+[А-Яа-я]+\\s+[А-Яа-я]+")) {
-        int spaceIndex = input.indexOf(' ');
-        int spaceIndex2 = input.lastIndexOf(' ');
-        System.out.println("Фамилия: " + (input.substring(0, spaceIndex)) + input.trim());
-        System.out.println("Имя: " + (input.substring(spaceIndex, spaceIndex2)) + input.trim());
-        System.out.println("Отчество: " + (input.substring(spaceIndex2)) + input.trim());
-        break;
-      } else {
-        System.out.println(" Вводимая строка не является ФИО ");
-        break;
+      if(isFullname == true){
+        input += System.lineSeparator();
+      }else {
+        System.out.println("Введенная строка не является ФИО");
       }
+
 
 
     }
@@ -39,5 +31,6 @@ public class Main {
 
 
 }
+
 
 
