8248b1e5c6d24ff3e48cf9f4deac58ff16d36807
Alexander <serkov.gcnet@mail.ru>
2021-06-02 18:47:56 +0300

    Реализация метода searchAndReplaceDiamonds

--- 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -8,19 +8,19 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String userInputText = scanner.nextLine();
 
-        String nonSecretText = searchAndReplaceDiamonds(userInputText, "***");
-        String nonSecretText2 = searchAndReplaceDiamonds(userInputText, "***");
+        String nonSecretText = searchAndReplaceDiamonds(userInputText, "<>");
 
-        System.out.println(nonSecretText + nonSecretText2);
+
+        System.out.println(nonSecretText);
     }
 
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
 
-        Pattern pattern = Pattern.compile()
-        Pattern pattern = Pattern.compile(digitsRegex);
+        Pattern pattern = Pattern.compile(".");
         Matcher matcher = pattern.matcher(text);
+
         String result = text;
 
         while (true) {
