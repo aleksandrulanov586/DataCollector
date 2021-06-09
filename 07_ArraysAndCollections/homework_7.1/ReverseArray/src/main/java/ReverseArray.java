f0597601dd98ca516829499eca8c3be445085059
Alexander <serkov.gcnet@mail.ru>
2021-06-09 15:46:44 +0300

    Реализация метода reverse, тесты выполнены

--- 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/ReverseArray.java
+++ 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/ReverseArray.java
@@ -1,19 +1,21 @@
 public class ReverseArray {
 
-    //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
-    public static String[] reverse (String[] strings){
+  //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
+  public static String[] reverse(String[] strings) {
 
+    int n = strings.length;
+    String text;
 
-
-
-        String[] words = strings.split(" ");
-        for (String word : words) {
-            System.out.println(word);
-        }
-return strings;
+    for (int i = 0; i < n / 2; i++) {
+      text = strings[n - i - 1];
+      strings[n - i - 1] = strings[i];
+      strings[i] = text;
     }
 
+    return strings;
+  }
 
-    }
+
+}
 
 
