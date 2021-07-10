8838831faa46b9c4be61d53f2fa851d61292a463
Alexander <serkov.gcnet@mail.ru>
2021-07-10 18:33:43 +0300

    Заполнение массива через цикл

--- 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -4,26 +4,26 @@ public class TwoDimensionalArray {
 
     public static char[][] getTwoDimensionalArray(int size) {
 
+
         //TODO: Написать метод, который создаст двумерный массив char заданного размера.
         // массив должен содержать символ symbol по диагоналям, пример для size = 3
         // [X,  , X]
         // [ , X,  ]
         // [X,  , X]
-        char[][] massa = new char[3][3];
-        massa[0][0] = symbol;
-        massa[0][2] = symbol;
-        massa[1][1] = symbol;
-        massa[2][0] = symbol;
-        massa[2][2] = symbol;
-for (int i = 0; i < 3; i++ ){
+size = 5;
+char[][] cross = new char[size][size];
 
-    for (int j = 0; j < 3; j++ ){
-        System.out.println(massa[j][i]);
+for (int i = 0; i < size; i++ ){
+
+
+    for (int j = 0; j < size; j++ ){
+       System.out.println(cross[symbol]);
 
     }
 }
 
 
         return new char[0][0];
+
     }
 }
