de55384d412878e51de7d884ab2666e7bcf5d0ef
Alexander <serkov.gcnet@mail.ru>
2021-08-22 08:20:06 +0300

    Реалищация метода generatePatientsTemperatures

--- 07_ArraysAndCollections/homework_7.1/Hospital/src/main/java/Hospital.java
+++ 07_ArraysAndCollections/homework_7.1/Hospital/src/main/java/Hospital.java
@@ -33,8 +33,8 @@ public class Hospital {
       double sum = 0;
       for (int j = 0; j < temperatureData.length; j++) {
         sum += temperatureData[j];
-        if(  ){
-
+        if( PatientTemperature > 36.6 || PatientTemperature  < 42.0 ){
+          HealthyPatients++;
         }
       }
 
