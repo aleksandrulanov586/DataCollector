75df8d7d269a5a6916a83a946dd0970b0734a804
Александр Серков <serkov.gcnet@mail.ru>
2021-08-25 16:50:27 +0300

    Реализация метода generatePatientsTemperatures

--- 07_ArraysAndCollections/homework_7.1/Hospital/src/main/java/Hospital.java
+++ 07_ArraysAndCollections/homework_7.1/Hospital/src/main/java/Hospital.java
@@ -1,52 +1,49 @@
 public class Hospital {
 
-  public static float[] generatePatientsTemperatures(int patientsCount) {
+    public static float[] generatePatientsTemperatures(int patientsCount) {
 
-    //TODO: напишите метод генерации массива температур пациентов
-    float[] patientsTemperatures = new float[patientsCount];
-
-
-     for (int i = 0; i < patientsTemperatures.length; i++){
-       float randomTemperatures = (float) (Math.random()*((20.6+10.5)+3.1))+3+1+1+10;
-       patientsTemperatures[i] = randomTemperatures;
-         getReport(patientsTemperatures);
-     }
-
-    return patientsTemperatures ;
+        //TODO: напишите метод генерации массива температур пациентов
+        float[] patientsTemperatures = new float[patientsCount];
 
 
-  }
+        for (int i = 0; i < patientsTemperatures.length; i++) {
+            float randomTemperatures = (float) (Math.random() * ((20.6 + 10.5) + 3.1)) + 3 + 1 + 1 + 10;
+            patientsTemperatures[i] = randomTemperatures;
+            getReport(patientsTemperatures);
+        }
 
-  public static String getReport(float[] temperatureData) {
+        return patientsTemperatures;
+
+
+    }
+
+    public static String getReport(float[] temperatureData) {
         /*
         TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
             а также температуры всех пациентов.
         */
 
-    int healthyPatients = 0;
-      float averageTemperature = 0;
-    float patientTemperature = 0;
+        int healthyPatients = 0;
+        float averageTemperature = 0;
+        float patientTemperature = 0;
 
-      for (int i = 0; i < temperatureData.length; i++){
-          if( patientTemperature > 36.6 || patientTemperature  < 36.9 ){
-              healthyPatients++;
-          }
-          if( true ){
-              temperatureData + temperatureData / temperatureData ;
+        for (int i = 0; i < temperatureData.length; i++) {
+            if (patientTemperature > 36.6 || patientTemperature < 36.9) {
+                healthyPatients++;
+            }
+            if (true) {
+                temperatureData + temperatureData / temperatureData;
 
-          }
-          temperatureData[i] = patientTemperature;
-      }
-
-
+            }
+            temperatureData[i] = patientTemperature;
+        }
 
 
+        String report =
+                "Температуры пациентов: " + patientTemperature +
+                        "\nСредняя температура: " + averageTemperature +
+                        "\nКоличество здоровых: " + healthyPatients;
 
-    String report =
-        "Температуры пациентов: " + patientTemperature +
-            "\nСредняя температура: " + averageTemperature +
-            "\nКоличество здоровых: " + healthyPatients;
-
-    return report;
-  }
+        return report;
+    }
 }
