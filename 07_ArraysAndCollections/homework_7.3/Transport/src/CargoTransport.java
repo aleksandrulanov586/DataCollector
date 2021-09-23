2b0850900c471f044783d23ca0004e085d5c7c5d
Александр Серков <serkov.gcnet@mail.ru>
2021-09-23 17:44:36 +0300

    Реализация класса Transport

---
+++ 07_ArraysAndCollections/homework_7.3/Transport/src/CargoTransport.java
@@ -0,0 +1,6 @@
+public class CargoTransport extends Transport {
+    public CargoTransport(boolean isPassenger, double weight, int wheelsCount, double maxSpeed) {
+        super(isPassenger, weight, wheelsCount, maxSpeed);
+    }
+
+}
