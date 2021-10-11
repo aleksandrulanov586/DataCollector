cb7ad91b8a0744eb2e4ca80abd4566d9fe83548f
Александр Серков <serkov.gcnet@mail.ru>
2021-10-11 17:49:42 +0300

    Реализация Класса TodoList (исправление)

--- 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
+++ 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
@@ -3,12 +3,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TodoList {
+    final static String ADDING_TO_END = "ADD\\s+\\D+\\s+";
+    final static String ADDING_TO_INDEX = "ADD\\s+.+";  // Сделал конастанты с регулярками  Я вот подумал написать вот такую регулярку как вы считаете она будет эффективной?"\\D[ADD]\\s+.+";
+    final static String REPLACE_WITH_INDEX = "EDIT\\s+\\d+\\s+.+";
+    final static String DELETING_BY_INDEX = "DELETE\\s+\\d+";
     ArrayList<String> todoList = new ArrayList<>();
 
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
 
-        Pattern pattern = Pattern.compile("ADD\\s+.+");
+        Pattern pattern = Pattern.compile(ADDING_TO_END);
         Matcher matcher = pattern.matcher(todo);
 
         if (matcher.matches()) {
@@ -19,8 +23,11 @@ public class TodoList {
     }
 
     public void add(int index, String todo) {
-
-
+        Pattern pattern = Pattern.compile(ADDING_TO_INDEX);
+        Matcher matcher = pattern.matcher(todo);
+        if (matcher.matches()) {
+            todoList.add(index, todo);
+        }
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
 
