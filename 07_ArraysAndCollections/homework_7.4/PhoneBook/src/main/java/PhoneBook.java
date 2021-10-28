b7c21855062a49d9845be5c4a6c64dbabf781556
Александр Серков <serkov.gcnet@mail.ru>
2021-10-28 17:56:18 +0300

    Реализация класса PhoneBook(исправление)

--- 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/PhoneBook.java
+++ 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/PhoneBook.java
@@ -4,16 +4,17 @@ import java.util.regex.Pattern;
 
 public class PhoneBook {
     public String NAME = "\\w+";
-    public String PHONES_BY_NAME = "\\w+\\s+\\d{11}";
+
     public String PHONE_NUMBER = "\\d{11}";
     public static String PHONES_BY_NAME = "\\w+\\s+.+\\s+\\d{11}";
+    public static String GET_NAME_BY_PHONE = "\\w+\\s+.+\\s+\\d{11}";
     HashMap<String, String> hashMap = new HashMap<>();
 
 
     public void addContact(String phone, String name) {
 
         if (name.matches(NAME) && phone.matches(PHONE_NUMBER)) {
-            System.out.println("Введите номер телефона для абонента" + name);
+
             hashMap.put(phone, name);
             System.out.println("Контакт сохранен!");
         } else {
@@ -28,11 +29,9 @@ public class PhoneBook {
 
     public String getNameByPhone(String phone) {
         for (int i = 0; 0 < hashMap.size(); i++) {
-            if (phone.matches(PHONES_BY_NAME)) {
-                System.out.println(hashMap.get(i));
-            } else {
-                return "";
-            }
+            System.out.println(hashMap.get(i));
+            String search = hashMap.get(phone);
+            System.out.println(search);
         }
         // формат одного контакта "Имя - Телефон"
         // если контакт не найдены - вернуть пустую строку
@@ -45,20 +44,25 @@ public class PhoneBook {
         for (int i = 0; 0 < hashMap.size(); i++) {
             if (name.matches(PHONES_BY_NAME)) {
                 System.out.println(hashMap.get(i));
-            } else {
-                return treeSet;
             }
             // формат одного контакта "Имя - Телефон"
             // если контакт не найден - вернуть пустой TreeSet
 
         }
+        return treeSet;
+    }
 
-        public Set<String> getAllContacts () {
+    public Set<String> getAllContacts() {
+        TreeSet<String> treeSet = new TreeSet<>();
+
+        for (int i = 0; 0 < hashMap.size(); i++) {
+
+            System.out.println(hashMap.get(i));
 
             // формат одного контакта "Имя - Телефон"
             // если контактов нет в телефонной книге - вернуть пустой TreeSet
-            return new TreeSet();
-        }
 
+        }
+        return treeSet;
     }
 }
