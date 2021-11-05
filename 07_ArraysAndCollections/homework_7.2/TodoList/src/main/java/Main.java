419d1fdf8af9b7caa50bac7ce6714fc3ca4915fa
Alexander <serkov.gcnet@mail.ru>
2021-11-05 12:40:37 +0300

    Реализация класса TodoList (иправление)

--- 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/Main.java
@@ -3,51 +3,54 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    public static String DELETING_BY_INDEX = "DELETE\\s+\\d+";
-    public static String TEAM_LIST = "LIST";
 
-    private static TodoList todoList = new TodoList();
+  public static String DELETING_BY_INDEX = "DELETE\\s+\\d+";
+  public static String TEAM_LIST = "LIST";
 
-    public static void main(String[] args) {
+  private static TodoList todoList = new TodoList();
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        input.split(input);
+  public static void main(String[] args) {
 
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
 
-        if (input.matches(todoList.ADDING_TO_END)) {
-            todoList.add(input);
-        }
-        if (input.matches(todoList.ADDING_BY_INDEX)) {
-            String[] index = input.split(" ", 2);
-            String indexText = index[1];
-            int parsInput = Integer.parseInt(indexText);
-            todoList.add(parsInput, input);
-        }
-        if (input.matches(todoList.REPLACE_WITH_INDEX)) {
-            String[] index = input.split(" ", 2);
-            String indexText = index[1];
-            int parsInput = Integer.parseInt(indexText);
-            todoList.edit(input, parsInput);
-
-        }
-        if (input.matches(DELETING_BY_INDEX)) {
-            String[] index = input.split(" ", 2);
-            String indexText = index[1];
-            int parsInput = Integer.parseInt(indexText);
-            todoList.delete(parsInput);
-        }
-        if (input.matches(TEAM_LIST)) {
-            System.out.println(todoList.getTodos());
-        }
-
-        todoList.add("work");
-        todoList.add(1, "java");
-        todoList.printTodos();
-
-        System.out.println(todoList);
-
-
-        // TODO: написать консольное приложение для работы со списком дел todoList
+    if (input.matches(todoList.ADDING_TO_END)) {
+      String[] add = input.split("ADD");
+      todoList.add(add[1]);
     }
+    if (input.matches(todoList.ADDING_BY_INDEX)) {
+
+      String[] componentTeam = input.split(" ", 3);
+      String indexText = componentTeam[1];
+      int parsInput = Integer.parseInt(indexText);
+
+      // int q  = (int) parsInput;
+      String TextСase = componentTeam[2];
+      todoList.add(parsInput, TextСase);
+    }
+    if (input.matches(todoList.REPLACE_WITH_INDEX)) {
+      String[] componentTeam = input.split(" ", 3);
+      String indexText = componentTeam[1];
+      int parsInput = Integer.parseInt(indexText);
+      todoList.edit(input, parsInput);
+
+    }
+    if (input.matches(DELETING_BY_INDEX)) {
+      String[] componentTeam = input.split(" ", 3);
+      String indexText = componentTeam[1];
+      int parsInput = Integer.parseInt(indexText);
+      todoList.delete(parsInput);
+    }
+    if (input.matches(TEAM_LIST)) {
+      System.out.println(todoList.getTodos());
+    }
+
+    todoList.add("work");
+    todoList.add(1, "java");
+    todoList.printTodos();
+
+    System.out.println(todoList);
+
+    // TODO: написать консольное приложение для работы со списком дел todoList
+  }
 }
--- 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
+++ 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
@@ -3,67 +3,64 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TodoList {
-    public String ADDING_BY_INDEX = "ADD\\s+\\d+\\s+.+";
-    public String ADDING_TO_END = "ADD\\s+.+";
-    public String REPLACE_WITH_INDEX = "EDIT\\s+\\d+\\s+";
+
+  public String ADDING_BY_INDEX = "ADD\\s+\\d+\\s+.+";
+  public String ADDING_TO_END = "ADD\\s+.+";
+  public String REPLACE_WITH_INDEX = "EDIT\\s+\\d+\\s+";
 
 
-    ArrayList<String> todoList = new ArrayList<>();
+  ArrayList<String> todoList = new ArrayList<>();
 
-    public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
+  public void add(String todo) {
+    // TODO: добавьте переданное дело в конец списка
 
+    todoList.add(todo);
 
-        todo.split("ADD");
-        todoList.add(todo);
+    System.out.println("Добавлено дело" + todo);
 
-        System.out.println("Добавлено дело" + todo);
+  }
 
+  public void add(int index, String todo) {
+
+    if (todo.matches(ADDING_BY_INDEX)) {
+
+      todoList.add(index, todo);
     }
+    // TODO: добавьте дело на указаный индекс,
+    //  проверьте возможность добавления
 
-    public void add(int index, String todo) {
+  }
 
-        if (todo.matches(ADDING_BY_INDEX)) {
-            todo.split("ADD");
-            todoList.add(index, todo);
-        }
-        // TODO: добавьте дело на указаный индекс,
-        //  проверьте возможность добавления
+  public void edit(String todo, int index) {
 
+    if (todo.matches(REPLACE_WITH_INDEX)) {
 
+      todoList.add(index, todo);
     }
+    // TODO: заменить дело на index переданным todo индекс,
+    //  проверьте возможность изменения
+  }
 
-    public void edit(String todo, int index) {
+  public void delete(int index) {
 
-        if (todo.matches(REPLACE_WITH_INDEX)) {
-            todo.split("EDIT");
-            todoList.add(index, todo);
-        }
-        // TODO: заменить дело на index переданным todo индекс,
-        //  проверьте возможность изменения
+    if (index < todoList.size()) {
+      todoList.remove(index);
     }
+    // TODO: удалить дело находящееся по переданному индексу,
+    //  проверьте возможность удаления дела
 
-    public void delete(int index) {
+  }
 
+  public ArrayList<String> getTodos() {
+    // TODO: вернуть список дел
+    return todoList;
+  }
 
-        if (index < todoList.size()) {
-            todoList.remove(index);
-        }
-        // TODO: удалить дело находящееся по переданному индексу,
-        //  проверьте возможность удаления дела
+  public void printTodos() {
+    for (String printLine : todoList) {
 
+      System.out.println(printLine);
     }
-
-    public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return todoList;
-    }
-
-    public void printTodos() {
-        for (int i = 0; i < todoList.size(); i++) {
-
-            System.out.println(todoList.get(i));
-        }
-        // напишите код которые печатает каждую строку todos
-    }
+    // напишите код которые печатает каждую строку todos
+  }
 }
