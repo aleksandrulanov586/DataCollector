d6a5105e9022b80c514502e3da828c0d7ca25d6a
Александр Серков <serkov.gcnet@mail.ru>
2021-11-08 17:26:16 +0300

    Дополнительная задача (исправление)

--- 07_ArraysAndCollections/homework_7.4/Set/src/Main.java
+++ 07_ArraysAndCollections/homework_7.4/Set/src/Main.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
+    public static String TEXT = ".+";
+    public static String PRINT_THE_LIST = "LIST";
+    public static SetCollections classCollections = new SetCollections();
 
-  public  static  SetCollections ClassCollections = new SetCollections();
-  public static void main(String[] args){
+    public static void main(String[] args) {
 
 
-    Scanner scanner = new Scanner(System.in);
-    while (true){
-      String input = scanner.nextLine();
-      if (input.matches(SetCollections.TEXT))
-        ClassCollections.add(input);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.matches(TEXT)) {
+                classCollections.add(input);
+            }
+            if (input.matches(PRINT_THE_LIST)) {
+                classCollections.list();
+            }
+        }
+
     }
-
-  }
 
 }
--- 07_ArraysAndCollections/homework_7.4/Set/src/SetCollections.java
+++ 07_ArraysAndCollections/homework_7.4/Set/src/SetCollections.java
@@ -3,19 +3,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SetCollections {
-  public static String TEXT = ".+";
-   ArrayList<String> collectionText = new ArrayList<>();
+
+    Set<String> collectionText = new HashSet<>();
 
 
-public void add (String text){
-  for (String s : collectionText){
-    if (s.matches(text) != text.matches(s)){
-      collectionText.add(text);
+    public void add(String text) {
 
-    }else {
-      System.out.println("Такой текст уже есть!" ) ;}
+        collectionText.add(text);
 
-  }
 
-}
+    }
+
+    public void list() {
+        for (String w : collectionText)
+            System.out.println(w);
+
+
+    }
 }
