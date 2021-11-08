2a0388afbfd618882a550b1aee919a425eb1765e
Александр Серков <serkov.gcnet@mail.ru>
2021-11-08 18:22:57 +0300

    Реализация класса PhoneBook (Исправление)

--- 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/Main.java
@@ -4,27 +4,31 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-  public static String PRINT_THE_LIST = "LIST";
-  public static String PRINT_THE_EXIT = "EXIT";
+    public static String PRINT_THE_LIST = "LIST";
+    public static String PRINT_THE_EXIT = "EXIT";
 
 
-  public static void main(String[] args) {
-    PhoneBook phoneBook = new PhoneBook();
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        Scanner scanner = new Scanner(System.in);
 
-    while (true) {
-      String input = scanner.nextLine();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.matches((input.matches(phoneBook.NAME) && input.matches(phoneBook.PHONE_NUMBER)) {
+                phoneBook.addContact(input, input);
+            }
 
-      if (input.matches(phoneBook.PHONES_BY_NAME)) {
-        phoneBook.getPhonesByName(input);
-      }
-      if (input.matches(PRINT_THE_LIST)) {
-        System.out.println(phoneBook.hashMap);
-      }
-      if (input.matches(PRINT_THE_EXIT)) {
-        return;
-      }
+            if (input.matches(phoneBook.PHONES_BY_NAME)) {
+                phoneBook.getPhonesByName(input);
+            }
 
+            if (input.matches(PRINT_THE_LIST)) {
+                System.out.println(phoneBook.hashMap);
+            }
+            if (input.matches(PRINT_THE_EXIT)) {
+                return;
+            }
+
+        }
     }
-  }
 }
--- 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/PhoneBook.java
+++ 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/PhoneBook.java
@@ -4,71 +4,74 @@ import java.util.regex.Pattern;
 
 public class PhoneBook {
 
-  public String NAME = "\\w+";
+    public String NAME = "\\w+";
 
-  public String PHONE_NUMBER = "\\d{11}";
-  public static String PHONES_BY_NAME = "\\w+\\s+.+\\s+\\d{11}";
-  public static String GET_NAME_BY_PHONE = "\\w+\\s+.+\\s+\\d{11}";
-  Map<String, String> hashMap = new HashMap<>();
+    public String PHONE_NUMBER = "\\d{11}";
+    public static String PHONES_BY_NAME = "\\w+\\s+.+\\s+\\d{11}";
+    // public static String GET_NAME_BY_PHONE = "\\w+\\s+.+\\s+\\d{11}";
+    Map<String, String> hashMap = new HashMap<>();
 
 
-  public void addContact(String phone, String name) {
+    public void addContact(String phone, String name) {
 
-    if (name.matches(NAME) && phone.matches(PHONE_NUMBER)) {
+        if (name.matches(NAME) && phone.matches(PHONE_NUMBER)) {
 
-      hashMap.put(phone, name);
-      System.out.println("Контакт сохранен!");
-    } else {
-      System.out.println("Неверный формат ввода");
+            hashMap.put(phone, name);
+            System.out.println("Контакт сохранен!");
+        } else {
+            System.out.println("Неверный формат ввода");
+        }
+
+        // проверьте корректность формата имени и телефона
+        // если такой номер уже есть в списке, то перезаписать имя абонента
+
     }
 
-    // проверьте корректность формата имени и телефона
-    // если такой номер уже есть в списке, то перезаписать имя абонента
+    public String getNameByPhone(String phone) {
 
-  }
+        for (Map.Entry<String, String> entry : hashMap.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (phone.equals(entry.getValue())) {
+                return "";
+            }
+            if (phone.equals(entry.getKey())) {
+                return null;
+            }
 
-  public String getNameByPhone(String phone) {
-
-    for (Map.Entry<String, String> entry : hashMap.entrySet()) {
-      String key = entry.getKey();
-      String value = entry.getValue();
-      if (phone.matches(value) != value.matches(NAME)) {
+        }
+        // формат одного контакта "Имя - Телефон"
+        // если контакт не найдены - вернуть пустую строку
         return "";
-      }
-      if (phone.matches(key) != key.matches(PHONE_NUMBER)) {
-        return null;
-      }
-
     }
-    // формат одного контакта "Имя - Телефон"
-    // если контакт не найдены - вернуть пустую строку
-    return "";
-  }
 
-  public Set<String> getPhonesByName(String name) {
-    TreeSet<String> treeSet = new TreeSet<>();
+    public Set<String> getPhonesByName(String name) {
+        TreeSet<String> treeSet = new TreeSet<>();
 
-    for (Map.Entry<String, String> entry : hashMap.entrySet()) {
-      if (name.matches(NAME)) {
-        String value = entry.getValue();
+        for (Map.Entry<String, String> entry : hashMap.entrySet()) {
+            String value = entry.getValue();
+            if (name.equals(entry.getValue())) {
+                String e = "";
 
-      }
-      // формат одного контакта "Имя - Телефон"
-      // если контакт не найден - вернуть пустой TreeSet
+            }
+            // формат одного контакта "Имя - Телефон"
+            // если контакт не найден - вернуть пустой TreeSet
 
+        }
+        return treeSet;
     }
-    return treeSet;
-  }
 
-  public Set<String> getAllContacts() {
-    TreeSet<String> treeSet = new TreeSet<>();
+    public Set<String> getAllContacts() {
+        TreeSet<String> treeSet = new TreeSet<>();
 
-    for (Map.Entry<String, String> entry : hashMap.entrySet()) {
+        for (Map.Entry<String, String> entry : hashMap.entrySet()) {
+            for (Map.Entry<String, String> treeentry : hashMap.entrySet()) {
 
-      // формат одного контакта "Имя - Телефон"
-      // если контактов нет в телефонной книге - вернуть пустой TreeSet
+            }
+            // формат одного контакта "Имя - Телефон"
+            // если контактов нет в телефонной книге - вернуть пустой TreeSet
 
+        }
+        return treeSet;
     }
-    return treeSet;
-  }
 }
