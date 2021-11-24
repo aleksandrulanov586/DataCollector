d461a05d85db6176a27fd0c94d02732eb9fa95ba
Александр Серков <serkov.gcnet@mail.ru>
2021-11-24 18:03:27 +0300

    Реализация класса CoolNumbers

--- 07_ArraysAndCollections/homework_7.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 07_ArraysAndCollections/homework_7.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -7,43 +7,45 @@ public class CoolNumbers {
 
 
     public static List<String> generateCoolNumbers() {
-        String AlphaNumericString = "А В Е К М Н О Р С Т У Х"
+        String letters = "АВЕКМНОРСТУХ";
+        String numbers = "0123456789";
 
-                + "0123456789";
-
-
-        StringBuilder sb = new StringBuilder(n);
-
-
-        for (int i = 0; i < AlphaNumericString.length(); i++) {
-
-
-            int index = (int) (AlphaNumericString.length() * Math.random());
-
-
-            // добавляем символ один за другим в конец sb
-
-            sb.append(AlphaNumericString
-
-                    .charAt(index));
-            return Collections.emptyList();
+        String NumbersLetters = letters + numbers;
+        for (int q = 0; q < letters.length(); q++) {
+            double GeneratingLetters1 = letters.length() * Math.random();
+            for (int w = 0; w < letters.length(); w++) {
+                double GeneratingLetters2 = letters.length() * Math.random();
+                for (int e = 0; e < letters.length(); e++) {
+                    double GeneratingLetters3 = letters.length() * Math.random();
+                    for (int r = 0; r < numbers.length(); r++) {
+                        int GeneratingNumber = (int) (numbers.length() * Math.random());
+                        for (int t = 0; t < numbers.length(); t++) {
+                            int GeneratingRegion = (int) (numbers.length() * Math.random());
+                        }
+                    }
+                }
+            }
         }
+
+
+        return Collections.emptyList();
     }
-        public static boolean bruteForceSearchInList (List < String > list, String number){
-            return false;
-        }
 
-        public static boolean binarySearchInList (List < String > sortedList, String number){
-            return false;
-        }
-
-
-        public static boolean searchInHashSet (HashSet < String > hashSet, String number){
-            return false;
-        }
-
-        public static boolean searchInTreeSet (TreeSet < String > treeSet, String number){
-            return false;
-        }
-
+    public static boolean bruteForceSearchInList(List<String> list, String number) {
+        return false;
     }
+
+    public static boolean binarySearchInList(List<String> sortedList, String number) {
+        return false;
+    }
+
+
+    public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
+        return false;
+    }
+
+    public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
+        return false;
+    }
+
+}
