a8c1632fadd71d6c067686441c18c3410797fb4a
Alexander <serkov.gcnet@mail.ru>
2021-12-22 14:12:26 +0300

    Реализация класса PhoneBook (исправление)

--- 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/Main.java
@@ -6,42 +6,47 @@ import java.util.regex.Pattern;
 public class Main {
 
 
-    public static String PRINT_THE_EXIT = "EXIT";
+  public static String PRINT_THE_EXIT = "EXIT";
 
 
-    public static void main(String[] args) {
-        PhoneBook phoneBook = new PhoneBook();
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    PhoneBook phoneBook = new PhoneBook();
+    Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            String input = scanner.nextLine();
+    while (true) {
+      String input = scanner.nextLine();
 
-            if (input.matches(phoneBook.PHONE_NUMBER)) {
-                  Set<String> phones = phoneBook.getPhonesByName(input);
-                phoneBook.getPhonesByName(input);
-                if (phones.isEmpty()) {
-                    System.out.println(phoneBook);
-                    // записываете номер и имя в тел книгу
-                } else {
-                    phoneBook.addContact(input);
-                    // печатайте полученный список
-                }else{
-                    if (phones.isEmpty()) {
-                        System.out.println(phoneBook);
-                    } else {
-                        phoneBook.addContact(input);
-                    }
-                }
-            }
-                if (input.equals("LIST")) {
-                    System.out.println(phoneBook.hashMap);
-                    continue;
-                }
-                if (input.matches(PRINT_THE_EXIT)) {
-                    return;
-                }
+      if (input.matches(phoneBook.PHONE_NUMBER)) {
 
-            }
+
+        if (phones.isEmpty()) {
+          System.out.println(phoneBook);
+          // записываете номер и имя в тел книгу
+        } else {
+          System.out.println("Введите имя!");
+          String input1 = scanner.nextLine();
+          // печатайте полученный список
         }
+      } else if() {
+
+        if (phones.isEmpty()) {
+          System.out.println(phoneBook);
+
+        } else {
+
+          phoneBook.addContact(input, input1);
+        }
+      }
+
+      if (input.equals("LIST")) {
+        System.out.println(phoneBook.hashMap);
+        continue;
+      }
+      if (input.matches(PRINT_THE_EXIT)) {
+        return;
+      }
+
     }
+  }
+}
 
