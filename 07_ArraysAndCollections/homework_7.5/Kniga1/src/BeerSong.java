de92ac057ac6d8594944da4c4f51ad227414ace9
Александр Серков <serkov.gcnet@mail.ru>
2021-12-24 17:38:23 +0300

    Реализация кода из книги

---
+++ 07_ArraysAndCollections/homework_7.5/Kniga1/src/BeerSong.java
@@ -0,0 +1,21 @@
+public class BeerSong {
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "бутылок (бутылки)";
+        while (beerNum > 0){
+            if (beerNum == 1 ){
+                word = "Бутылка";
+            }
+            System.out.println(beerNum + " " + word + " " + "пива на стене");
+            System.out.println(beerNum + " " + word + " " +"пива.");
+            System.out.println(beerNum + " " + word + " " +"Возьми одну");
+            System.out.println(beerNum + " " + word + " " + "Пусти по кругу");
+            beerNum = beerNum - 1;
+            if (beerNum > 0){ // тут ошибка лишний блок кода потому как есть выши
+                System.out.println(beerNum + " " + word + " " + "Пива на стене");
+            }else{
+                System.out.print(beerNum + " " + word + " " + "Нет бутылок пива на стене");
+            }
+        }
+    }
+}
