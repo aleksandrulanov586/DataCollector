37a6111df4cdc2d54ed42cfb7d4a08c77bb93220
Alexander <serkov.gcnet@mail.ru>
2021-12-25 15:41:40 +0300

    Реализация класса (CoolNumbers)

--- 07_ArraysAndCollections/homework_7.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 07_ArraysAndCollections/homework_7.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -11,14 +11,14 @@ public class CoolNumbers {
 
     for (char letter1 : LETTERS.toCharArray()) {
 
-      for (int number = 1; number < 9; number++) {
+      for (int number = 1; number < 199; number++) {
 
         for (char letter2 : LETTERS.toCharArray()) {
 
           for (char letter3 : LETTERS.toCharArray()) {
 
             for (int region = 1; region < 199; region++) {
-              arrayList.add(String.valueOf(letter1 + number + letter2 + letter3 + region));
+              arrayList.add("" + letter1 + "" + number + "" + letter2 + letter3 + "" + region);
 
             }
           }
@@ -31,15 +31,11 @@ public class CoolNumbers {
 
   public static boolean bruteForceSearchInList(List<String> list, String number) {
 
-    if (arrayList.contains(number)) {
-      System.out.println(arrayList);
-    }
-
-    return false;
+    return arrayList.contains(number);
   }
 
   public static boolean binarySearchInList(List<String> sortedList, String number) {
-    return false;
+    return arrayList.sort(number);
   }
 
 
