a7de73e821e7a626efa5a9bd2db330e3d543b016
Александр Серков <serkov.gcnet@mail.ru>
2021-12-27 18:51:17 +0300

    Реализация класса PhoneBook

--- 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/Main.java
@@ -6,46 +6,50 @@ import java.util.regex.Pattern;
 public class Main {
 
 
-  public static String PRINT_THE_EXIT = "EXIT";
+    public static String PRINT_THE_EXIT = "EXIT";
 
 
-  public static void main(String[] args) {
-    PhoneBook phoneBook = new PhoneBook();
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.matches(phoneBook.PHONE_NUMBER)) {
-        Set<String> phones = phoneBook.getPhonesByName(input);
-        for (String phone : phones) {
-          if (phone.isEmpty()) {
-            System.out.println(phone);
-            // записываете номер и имя в тел книгу
-          } else {
-            System.out.println("Введите имя!");
-            String input1 = scanner.nextLine();
-            // печатайте полученный список
-          }
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.matches(phoneBook.PHONE_NUMBER)) {
+                Set<String> phones = phoneBook.getPhonesByName(input);
+                if (phones.isEmpty()) {
+
+                    // записываете номер и имя в тел книгу
+                    for (String phone : phones) {
+                        System.out.println(phone);
+
+                    }
+                    System.out.println("Введите имя!");
+                    String input1 = scanner.nextLine();
+                } else {
+                    phoneBook.addContact(input);
+
+                    // печатайте полученный списо
+                }
+            } else if (input.matches(phoneBook.NAME)) {
+                String name = phoneBook.getNameByPhone(input);
+
+                if (name.isEmpty()) {
+                    System.out.println(phoneBook);
+
+                } else {
+                    phoneBook.addContact(input, input);
+                }
+            }
+
+
+            if (input.equals("LIST")) {
+                System.out.println(phoneBook.hashMap);
+                continue;
+            }
+            if (input.matches(PRINT_THE_EXIT)) {
+                return;
+            }
         }
-        else if (input.matches(phoneBook.NAME)) {
-          String name = phoneBook.getNameByPhone(input);
-          for (String s : name) {
-          }
-          if (name.isEmpty()) {
-            System.out.println(phoneBook);
-
-          } else {
-            phoneBook.addContact(input, name);
-          }
-        }
-      }
-      if (input.equals("LIST")) {
-        System.out.println(phoneBook.hashMap);
-        continue;
-      }
-      if (input.matches(PRINT_THE_EXIT)) {
-        return;
-      }
     }
-  }
 }
 
