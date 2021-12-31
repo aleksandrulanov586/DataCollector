c05aee7bb18a625da0de61ebede9e8c52fb03f69
Alexander <serkov.gcnet@mail.ru>
2021-12-31 16:57:21 +0300

    lesson 1-9 done

--- 07_ArraysAndCollections/homework_7.5/Kniga/src/Test.java
+++ 07_ArraysAndCollections/homework_7.5/Kniga/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
         int y = 0;
 
       while (x < 5) {
-          //y = x - y;
+         // y = x - y;
           y = y + x;
          /**   y = y + 2;//бесконечность
              if (y < 5) {
@@ -13,14 +13,14 @@ public class Test {
                      x--;
                  }
              }
-             y = y + 2;
-             /** y = y + 2; //  02 - 14 - 25 - 36 -  47 -
+             /**y = y + 2;
+              y = y + 2; //  02 - 14 - 25 - 36 -  47 -
          if (y > 4) {
          y--;
-         }
+         }**/
          System.out.println(x + " " + y + " ");
          x++;
-         }**/
+         }
 
 
     }
