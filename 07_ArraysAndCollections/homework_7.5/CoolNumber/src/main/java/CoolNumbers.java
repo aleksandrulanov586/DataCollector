ec60dcadb4ca906cef3cd9072134e3da46e482a3
Александр Серков <serkov.gcnet@mail.ru>
2022-01-11 19:12:56 +0300

    Реализация метода CoolNumbers

--- 07_ArraysAndCollections/homework_7.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 07_ArraysAndCollections/homework_7.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -2,52 +2,52 @@ import java.util.*;
 
 public class CoolNumbers {
 
-  public static ArrayList<String> arrayList = new ArrayList<>();
+    public static ArrayList<String> arrayList = new ArrayList<>();
 
-  public static String LETTERS = "АВЕКМНОРСТУХ";
+    public static String LETTERS = "АВЕКМНОРСТУХ";
 
-  public static String NUMBER = "0123456789";
-
-
-  public static List<String> generateCoolNumbers() {
-
-    for (char letter1 : LETTERS.toCharArray()) {
-      for (int number = 1; number < 9; number++) {
-        for (char letter2 : LETTERS.toCharArray()) {
-          for (char letter3 : LETTERS.toCharArray()) {
-            for (int region = 1; region < 199; region++) {
-              String result = String.format("" + letter1 + number + letter2 + letter3 + region);
-              arrayList.add(result);
+    public static String NUMBER = "0123456789";
 
 
+    public static List<String> generateCoolNumbers() {
+
+        for (char letter1 : LETTERS.toCharArray()) {
+            for (int number = 1; number < 9; number++) {
+                for (char letter2 : LETTERS.toCharArray()) {
+                    for (char letter3 : LETTERS.toCharArray()) {
+                        for (int region = 1; region < 199; region++) {
+                            String result = letter1 + String.format("%03d", number) + letter2 + letter3 + region;
+                            arrayList.add("" + result);
+
+
+                        }
+                    }
+                }
             }
-          }
         }
-      }
+
+        return arrayList;
     }
 
-    return arrayList;
-  }
+    public static boolean bruteForceSearchInList(List<String> list, String number) {
+        return arrayList.contains(number);
+    }
 
-  public static boolean bruteForceSearchInList(List<String> list, String number) {
-    return arrayList.contains(number);
-  }
+    public static boolean binarySearchInList(List<String> sortedList, String number) {
 
-  public static boolean binarySearchInList(List<String> sortedList, String number) {
+        //Collections.sort(arrayList);
 
-    //Collections.sort(arrayList);
-
-    //Collections.binarySearch(sortedList, number);
-    return Collections.binarySearch(sortedList,number);
-  }
+        //Collections.binarySearch(sortedList, number);
+        return Collections.binarySearch(sortedList, number) >= 0 ? number : sortedList;
+    }
 
 
-  public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-    return false;
-  }
+    public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
+        return false;
+    }
 
-  public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-    return false;
-  }
+    public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
+        return false;
+    }
 
 }
