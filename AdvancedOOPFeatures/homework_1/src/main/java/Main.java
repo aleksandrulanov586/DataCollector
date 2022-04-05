dab339cb4876decee01547c5e4f6203c91fb75a9
Александр Серков <serkov.gcnet@mail.ru>
2022-04-05 10:32:49 +0300

    Реализация метода sortBySalaryAndAlphabet

--- AdvancedOOPFeatures/homework_1/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_1/src/main/java/Main.java
@@ -1,8 +1,7 @@
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
-public class Main implements Comparator<Employee> {
+public class Main {
 
     public static final String STAFF_TXT = "data/staff.txt";
 
@@ -11,19 +10,19 @@ public class Main implements Comparator<Employee> {
         System.out.println(staff);
     }
 
-    public static void sortBySalaryAndAlphabet(List<Employee> staff) {
+    public static int sortBySalaryAndAlphabet(List<Employee> staff) {
 
         Collections.sort(staff, (employee, t1) -> {
             employee.getSalary().compareTo(t1.getSalary());
-            employee.getName().compareTo(t1.getName());
+            return employee.getName().compareTo(t1.getName());
+
+
+        });
 
 
         //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
-    }
 
 
-    @Override
-    public int compare(Employee o1, Employee o2) {
-        return Integer.compare(o1.getSalary(), o2.getSalary());
+        return 0;
     }
 }
