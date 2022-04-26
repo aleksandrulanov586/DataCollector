f19d733d63220db5988ccc72b7823148cfd45d34
Александр Серков <serkov.gcnet@mail.ru>
2022-04-26 16:16:38 +0300

    Реализация метода findPlanesLeavingInTheNextTwoHours

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,7 +1,13 @@
+import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +16,23 @@ public class Main {
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+        LocalDateTime dateNow = LocalDateTime.now();
+
+        return airport.getTerminals().stream()
+                .flatMap(terminal -> terminal.getFlights().stream())
+                .filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE))
+                .filter(flight -> modifiedDate(flight)
+                        .isAfter(dateNow) && modifiedDate(flight)
+                        .isBefore(dateNow.plusHours(2)))
+                .collect(Collectors.toList());
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+
     }
+
+    public static LocalDateTime modifiedDate(Flight date) {
+        return LocalDateTime
+                .ofInstant(date.getDate().toInstant(), ZoneId.systemDefault());
+    }
+
 
 }
