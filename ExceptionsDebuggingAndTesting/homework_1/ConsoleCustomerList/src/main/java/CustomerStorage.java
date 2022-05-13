a7aafdb2a195acbbd48ac26a721e594b64586bd6
Александр Серков <serkov.gcnet@mail.ru>
2022-05-13 15:54:40 +0300

    Реализация метода addCustomer

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.lang.reflect.UndeclaredThrowableException;
+import java.util.Arrays;
 import java.util.EmptyStackException;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,23 +17,22 @@ public class CustomerStorage {
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
+        String w = "//d";
+        String[] components = data.split("\\s+");
+        String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+        storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+        if (name.equals(w)) {
+            throw new ArrayStoreException();
+        } else if (Arrays.stream(components).allMatch(s -> s.length() >= 4)) {
+            throw new ArrayStoreException();
+        }
 
-        try {   String[] components = data.split("\\s+");
-           String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME] ;
 
-           storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
-       }catch (ArrayStoreException | IndexOutOfBoundsException ignored){
-
-       }
     }
 
-
-
-
-
-
-
     public void listCustomers() {
+
+
         storage.values().forEach(System.out::println);
     }
 
