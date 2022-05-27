af378659b0625465ba82ab493de044c9c0ad8f6e
Александр Серков <serkov.gcnet@mail.ru>
2022-05-27 17:38:41 +0300

    Реализация class RouteCalculatorTest

--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/java/RouteCalculatorTest.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/java/RouteCalculatorTest.java
@@ -1,5 +1,11 @@
 import junit.framework.TestCase;
-
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class RouteCalculatorTest extends TestCase {
+
+@Test
+    public void  testgetRouteOnTheLine() throws Exception{
+    assertEquals
+    }
 
 }
