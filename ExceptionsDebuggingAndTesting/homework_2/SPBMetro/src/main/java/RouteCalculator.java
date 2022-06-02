6825a1c7358569cf4b1a800646f8f0d2566bb81e
Александр Серков <serkov.gcnet@mail.ru>
2022-06-02 18:01:36 +0300

    Реализация class RouteCalculatorTest

--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/java/RouteCalculator.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/java/RouteCalculator.java
@@ -1,94 +1,97 @@
-import core.Station;
+import core.Station; // имортируем пакеты
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
-public class RouteCalculator {
-    private final StationIndex stationIndex;
+public class RouteCalculator { // создаем класс
 
-    private static final double INTER_STATION_DURATION = 2.5;
+    private static final StationIndex stationIndex; // ссылка на обьект
+
+    private static final double INTER_STATION_DURATION = 2.5; // константа со значением
     private static final double INTER_CONNECTION_DURATION = 3.5;
 
-    public RouteCalculator(StationIndex stationIndex) {
-        this.stationIndex = stationIndex;
+    public RouteCalculator(StationIndex stationIndex) { // конструктор с аргемунтом класса stationIndex
+        this.stationIndex = stationIndex; // this говорит о том что имеется ввиду именно эта переменная
     }
 
 
 
-    public List<Station> getShortestRoute(Station from, Station to) {
-        List<Station> route = getRouteOnTheLine(from, to);
-        if (route != null) {
-            return route;
+    public static List<Station> getShortestRoute(Station from, Station to) { // публичный метод который возвращает коллекцию (джинерик)
+        List<Station> route = getRouteOnTheLine(from, to); // ссылка на коллекцию( джинерик ) передача в метод с аргументами
+        if (route != null) { // проверка евляется ли коллекция которая переданныя в метод пустой
+            return route; // если нет то вернуть коллекцию
         }
 
-        route = getRouteWithOneConnection(from, to);
-        if (route != null) {
-            return route;
+        route = getRouteWithOneConnection(from, to); // после этого перадаем коллекцию в другой метод
+        if (route != null) { // проверка евляется ли переданная коллекция в другой метод пустой
+            return route;// если нет то вернуть коллекцию
         }
 
-        route = getRouteWithTwoConnections(from, to);
-        return route;
+        route = getRouteWithTwoConnections(from, to); // еще одна ссылка на передачу в третий метод
+        return route; // и после этого возвращаем его
     }
 
-    public static double calculateDuration(List<Station> route) {
-        double duration = 0;
-        Station previousStation = null;
-        for (int i = 0; i < route.size(); i++) {
-            Station station = route.get(i);
-            if (i > 0) {
-                duration += previousStation.getLine().equals(station.getLine()) ?
-                        INTER_STATION_DURATION : INTER_CONNECTION_DURATION;
+    public static double calculateDuration(List<Station> route) { // создаем статический метод который возвращает значение с плавующий точкой с аргуметом коллекции
+        double duration = 0; // создаем переменную со значением 0
+        Station previousStation = null; // создаем переменную со значением null
+        for (int i = 0; i < route.size(); i++) { // создаем массив перебора по коллекции аргумента
+            Station station = route.get(i); // создаем ссылку на обьект и ложем ее в коллекцию аргумента которая будет возвращать колличество стацний
+            if (i > 0) { // проверка пока переменная в цикле равна больше 0
+                duration += previousStation.getLine().equals(station.getLine())  ? // то ложится в переменную сравнения null с 0
+                        INTER_STATION_DURATION : INTER_CONNECTION_DURATION; // если совпадения тру то ложим в переменную  2.5 если нет то 3.5
             }
-            previousStation = station;
+            previousStation = station; // null присваивается и ложим в коллекцию? Не совсем понятно зачем?
         }
-        return duration;
+        return duration; // возвращаем значение переменной после цикла
     }
 
-    private List<Station> getRouteOnTheLine(Station from, Station to) {
-        if (!from.getLine().equals(to.getLine())) {
-            return null;
+    private static List<Station> getRouteOnTheLine(Station from, Station to) {
+
+        if (!from.getLine().equals(to.getLine())) { // проверка если не откуда и куда не одинаковые
+            return null; // то возвращаем null
         }
-        List<Station> route = new ArrayList<>();
-        List<Station> stations = from.getLine().getStations();
+
+        List<Station> route = new ArrayList<>(); // создаем новую коллекцию
+        List<Station> stations = from.getLine().getStations(); // ссылка на коллекцию с возвращением станции и линии
         int direction = 0;
-        for (Station station : stations) {
-            if (direction == 0) {
-                if (station.equals(from)) {
-                    direction = 1;
-                } else if (station.equals(to)) {
-                    direction = -1;
+        for (Station station : stations) { // цикл foreach
+            if (direction == 0) { // проверка если направления равна 0
+                if (station.equals(from)) { // то мы сравниваем станцию откуда
+                    direction = 1; // если две проверки совпали то направления равна 0
+                } else if (station.equals(to)) { // проверка сравнения станции и куда
+                    direction = -1; // если совпала то направления - 1
                 }
             }
 
-            if (direction != 0) {
-                route.add(station);
+            if (direction != 0) { //если направление на равна 0
+                route.add(station); // то добавляем в коллекцию станцию
             }
 
-            if ((direction == 1 && station.equals(to)) ||
-                    (direction == -1 && station.equals(from))) {
-                break;
+            if ((direction == 1 && station.equals(to)) ||// если направление равно 1 ии станция равна равна направлению куда
+                    (direction == -1 && station.equals(from))) {  // или направления равна - 1 и станция станция равна откуда
+                break;        // то конец выполнения кода
             }
         }
-        if (direction == -1) {
-            Collections.reverse(route);
+        if (direction == -1) { // если направления равна минус 1
+            Collections.reverse(route); // то меняем порядок коллекции на обратный
         }
         return route;
     }
 
-    private List<Station> getRouteWithOneConnection(Station from, Station to) {
-        if (from.getLine().equals(to.getLine())) {
+    private static List<Station> getRouteWithOneConnection(Station from, Station to) {
+        if (from.getLine().equals(to.getLine())) { // если пункт назначения откуда и куда равна, то возвращаем null
             return null;
         }
 
         List<Station> route = new ArrayList<>();
 
-        List<Station> fromLineStations = from.getLine().getStations();
+        List<Station> fromLineStations = from.getLine().getStations();// ссылка на коллекцию станцию передаем два метода
         List<Station> toLineStations = to.getLine().getStations();
-        for (Station srcStation : fromLineStations) {
-            for (Station dstStation : toLineStations) {
-                if (isConnected(srcStation, dstStation)) {
+        for (Station srcStation : fromLineStations) { // цикл на первую ссылку перебор "откуда"
+            for (Station dstStation : toLineStations) {// цикл на вторую ссылку перебор "куда"
+                if (isConnected(srcStation, dstStation)) { //
                     ArrayList<Station> way = new ArrayList<>();
                     way.addAll(getRouteOnTheLine(from, srcStation));
                     way.addAll(getRouteOnTheLine(dstStation, to));
@@ -107,7 +110,7 @@ public class RouteCalculator {
         return connected.contains(station2);
     }
 
-    private List<Station> getRouteViaConnectedLine(Station from, Station to) {
+    private static List<Station> getRouteViaConnectedLine(Station from, Station to) {
         Set<Station> fromConnected = stationIndex.getConnectedStations(from);
         Set<Station> toConnected = stationIndex.getConnectedStations(to);
         for (Station srcStation : fromConnected) {
@@ -120,7 +123,7 @@ public class RouteCalculator {
         return null;
     }
 
-    private List<Station> getRouteWithTwoConnections(Station from, Station to) {
+    private static List<Station> getRouteWithTwoConnections(Station from, Station to) {
         if (from.getLine().equals(to.getLine())) {
             return null;
         }
--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/java/RouteCalculatorTest.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/java/RouteCalculatorTest.java
@@ -11,25 +11,37 @@ import static org.junit.Assert.*;
 
 public class RouteCalculatorTest extends TestCase {
 
+
     List<Station> stations;
 
-    @Test
-    public void setUp() throws Exception {
+    @Override
+    protected void setUp() throws Exception {
         stations = new ArrayList<>();
-        Line line = new Line(1, "1");
-        Line line2 = new Line(2, "2");
-        Line line3 = new Line(3, "3");
-        stations.add(new Station("AAsasS", line));
-        stations.add(new Station("AAsasS", line2));
-        stations.add(new Station("AAsasS", line3));
+        Line line = new Line(1, "Первая");
+        Line line2 = new Line(2, "Вторая");
+
+        stations.add(new Station("Петровская", line));
+        stations.add(new Station("Арбузная", line));
+        stations.add(new Station("Морковна", line2));
+        stations.add(new Station("Морковна", line2));
 
     }
 
-    public void testgetShortestRoute() throws Exception {
-        double current = RouteCalculator.ShortestRoute();
-        double t = 3;
-        assertEquals(t, current);
+    @Test
+    public void calculateDuration() throws Exception {
+
+        double actual = RouteCalculator.getShortestRoute();
+        double expected = 8.5;
+        assertEquals(expected, actual);
 
 
     }
+
+    @Override
+    protected void tearDown() throws Exception {
+
+    }
+
 }
+
+
