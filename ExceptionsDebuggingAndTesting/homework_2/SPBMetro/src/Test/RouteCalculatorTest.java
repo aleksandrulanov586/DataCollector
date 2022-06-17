9cbb2ba318918c8581dd8281209d28898652f276
Александр Серков <serkov.gcnet@mail.ru>
2022-06-17 17:52:20 +0300

    Реализация RouteCalculatorTest

--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/Test/RouteCalculatorTest.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/Test/RouteCalculatorTest.java
@@ -13,51 +13,48 @@ public class RouteCalculatorTest extends TestCase {
     RouteCalculator routeCalculator = new RouteCalculator(new StationIndex());
 
 
+
     List<Station> stations;
-    List<Station> stations1;
+    List<Station> stations2;
 
     @Override
     protected void setUp() throws Exception {
         stations = new ArrayList<>();
-        stations1 = new ArrayList<>();
+        stations2 = new ArrayList<>();
         Line line = new Line(1, "Первая");
         Line line2 = new Line(2, "Вторая");
         Line line3 = new Line(3, "Вторая");
-
+Station station = new Station("Петровская", line);
         stations.add(new Station("Петровская", line));
-        stations1.add(new Station("Морковна", line2));
+        stations2.add(new Station("Морковна", line2));
         stations.add(new Station("Морковна", line3));
 
     }
 
 
     public void testcalculateDuration() throws Exception {
+        Line line = new Line(1, "Первая");
+        Line line2 = new Line(2, "Вторая");
+        Line line3 = new Line(3, "Вторая");
+        Station station = new Station("Петровская", line);
+        Station station2 = new Station("Петровская", line);
+       String station1 = "Морковна";
+        String station2  =   "Петровская";
 
-
-        Station Station = null;
-        String station1 = (Station) + "Петровская";
-
-        Station actual = (Station) routeCalculator.getShortestRoute((core.Station) stations, (core.Station) stations1);
+        // Station Station = " dsdsad";
+        Station actual = routeCalculator.getShortestRoute( station ,  station2);
 
         assertEquals(station1, actual);
 
 
     }
 
-    public void testgetShortestRouteReflection() throws Exception {
-        try {
-
-            Method method = RouteCalculator.class.getDeclaredMethod("getShortestRoute", Station.class);
-            method.setAccessible(true);
-            assertEquals(method.invoke(routeCalculator.toString(), " "));
-        } catch (Exception exception) {
-            e
-        }
 
 
-    }
 
 
+
+    
     @Override
     protected void tearDown() throws Exception {
 
