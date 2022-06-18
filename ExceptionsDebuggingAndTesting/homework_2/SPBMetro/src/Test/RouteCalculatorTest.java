98b8d34556b40c9b1c6aa5fc89d929dd1b424939
Alexander <serkov.gcnet@mail.ru>
2022-06-18 19:29:13 +0300

    Ошибки

--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/Test/RouteCalculatorTest.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/Test/RouteCalculatorTest.java
@@ -15,18 +15,18 @@ public class RouteCalculatorTest extends TestCase {
 
 
     List<Station> stations;
-    List<Station> stations2;
+
 
     @Override
     protected void setUp() throws Exception {
         stations = new ArrayList<>();
-        stations2 = new ArrayList<>();
+
         Line line = new Line(1, "Первая");
         Line line2 = new Line(2, "Вторая");
         Line line3 = new Line(3, "Вторая");
 Station station = new Station("Петровская", line);
         stations.add(new Station("Петровская", line));
-        stations2.add(new Station("Морковна", line2));
+        stations.add(new Station("Морковна", line2));
         stations.add(new Station("Морковна", line3));
 
     }
@@ -35,14 +35,15 @@ Station station = new Station("Петровская", line);
     public void testcalculateDuration() throws Exception {
         Line line = new Line(1, "Первая");
         Line line2 = new Line(2, "Вторая");
-        Line line3 = new Line(3, "Вторая");
-        Station station = new Station("Петровская", line);
-        Station station2 = new Station("Петровская", line);
-       String station1 = "Морковна";
-        String station2  =   "Петровская";
+        Line line3 = new Line(3, "Третья");
 
-        // Station Station = " dsdsad";
-        Station actual = routeCalculator.getShortestRoute( station ,  station2);
+        Station station1 = new Station("Петровская", line);
+        Station station2 = new Station("Петровская", line2);
+       Station station3 = new Station("Морковна",line3 );
+
+
+
+        Station actual = (Station) routeCalculator.getShortestRoute( station1 ,  station2);
 
         assertEquals(station1, actual);
 
