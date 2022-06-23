5f6cd2a625b30b7e5b362afbcdf25bc0d3367dd1
Александр Серков <serkov.gcnet@mail.ru>
2022-06-23 15:44:48 +0300

    Реализация теста

--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/test/java/RouteCalculatorTest.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -20,43 +20,33 @@ public class RouteCalculatorTest extends TestCase {
     protected void setUp() throws Exception {
 
 
-        /**Line line = new Line(1, "Первая");
-         Line line2 = new Line(2, "Вторая");
-         Line line3 = new Line(3, "Третья");
-         Line line4 = new Line(4, "Четвертая");
-
-
-         Station station1 = new Station("Петровская", line);
-         Station station2 = new Station("Петровская", line2);
-         Station station3 = new Station("Морковна", line3);
-         Station station4 = new Station("Морковна", line4);
-         testcalculateDuration(station1, station2, station3, station4);**/
-
-    }
-
-
-    public void testcalculateDuration() throws Exception {
-
-
         Line line = new Line(1, "Первая");
         Line line2 = new Line(2, "Вторая");
         Line line3 = new Line(3, "Третья");
-        Line line4 = new Line(4, "Четвертая");
+
 
         Station station1 = new Station("Петровская", line);
         Station station2 = new Station("Петровская", line2);
         Station station3 = new Station("Морковна", line3);
-        Station station4 = new Station("Морковна", line4);
+        Station station4 = new Station("Морковна", line3);
+
+        List<Station> transfer1 = new ArrayList<>();
+        List<Station> transfer2 = new ArrayList<>();
+        stationIndex.addStation(station1);
+        stationIndex.addStation(station2);
+        stationIndex.addStation(station3);
+        stationIndex.addStation(station4);
+
+        stationIndex.addConnection(transfer1);
+        stationIndex.addConnection(transfer2);
+testcalculateDuration(station1, station2, station3, station4 );
+    }
 
 
-        List<Station> connect1 = new ArrayList<>();
-        List<Station> connect2 = new ArrayList<>();
+    public void testcalculateDuration(Station station1,Station station2,Station station3,Station station4 ) throws Exception {
 
-        stationIndex.addConnection(connect1);
-        stationIndex.addConnection(connect2);
 
-        route.add(station1);
-        route.add(station2);
+
 
         List<Station> actual = routeCalculator.getShortestRoute(station1, station4);
         List<Station> expected = List.of(station1, station2, station3, station4);
