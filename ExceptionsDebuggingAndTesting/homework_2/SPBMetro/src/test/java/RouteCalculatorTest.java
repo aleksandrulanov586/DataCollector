df31e94e325f9b9e751deb69e5c88fb6da737794
Александр Серков <serkov.gcnet@mail.ru>
2022-06-27 16:57:19 +0300

    Реализация теста

--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/test/java/RouteCalculatorTest.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -12,8 +12,8 @@ public class RouteCalculatorTest extends TestCase {
     StationIndex stationIndex = new StationIndex();
     RouteCalculator routeCalculator = new RouteCalculator(stationIndex);
 
-
-    List<Station> route = new ArrayList<>();
+    List<Station> transfer1 = new ArrayList<>();
+    List<Station> transfer2 = new ArrayList<>();
 
 
     @Override
@@ -29,19 +29,32 @@ public class RouteCalculatorTest extends TestCase {
         Station station2 = new Station("Достоевская", line3);
         Station station3 = new Station("Сенная площадь", line2);
         Station station4 = new Station("Гостиный двор", line3);
+        Station station5 = new Station("Девяткино", line3);
 
-        List<Station> transfer1 = new ArrayList<>();
-        List<Station> transfer2 = new ArrayList<>();
-        transfer1.add(station2);
-        transfer2.add(station3);
+        Station station6 = new Station("Гражданский проспект", line);
+        Station station7 = new Station("Академическая", line3);
+        Station station8 = new Station("Политехническая", line2);
+        Station station9 = new Station("Площадь Мужества", line3);
+        Station station10 = new Station("Лесная", line3);
+
+        Station station11 = new Station("Выборгская", line);
+        Station station12 = new Station("Площадь Ленина", line3);
+        Station station13 = new Station("Чернышевская", line2);
+        Station station14 = new Station("Площадь Восстания", line3);
+        Station station15 = new Station("Владимирская", line3);
         stationIndex.addStation(station1);
         stationIndex.addStation(station2);
         stationIndex.addStation(station3);
         stationIndex.addStation(station4);
+        stationIndex.addStation(station5);
+        stationIndex.addStation(station6);
+        stationIndex.addStation(station7);
+        stationIndex.addStation(station8);
 
         stationIndex.addConnection(transfer1);
         stationIndex.addConnection(transfer2);
-
+        transfer1.add(station2);
+        transfer2.add(station3);
     }
 
 
@@ -52,12 +65,8 @@ public class RouteCalculatorTest extends TestCase {
         Line line2 = stationIndex.getLine(2);
         Line line3 = stationIndex.getLine(3);
 
-        Station station1 = new Station("Лесная", line);
-        Station station2 = new Station("Достоевская", line2);
-        Station station3 = new Station("Сенная площадь", line2);
-        Station station4 = new Station("Гостиный двор", line3);
 
-        List<Station> actual = routeCalculator.getShortestRoute(station1, station2);
+        List<Station> actual = routeCalculator.getShortestRoute(stationIndex.getLine(1));
         List<Station> expected = List.of(station1, station2, station3, station4);
         assertEquals(expected, actual);
 
