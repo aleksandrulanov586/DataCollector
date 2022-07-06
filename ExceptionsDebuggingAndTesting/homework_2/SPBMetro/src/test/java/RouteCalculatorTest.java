103d7b8310adab6322f45c9b6dfb73a2d708833c
Александр Серков <serkov.gcnet@mail.ru>
2022-07-06 16:44:12 +0300

    Реализация теста

--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/test/java/RouteCalculatorTest.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -24,7 +24,7 @@ public class RouteCalculatorTest extends TestCase {
         Line line = new Line(1, "Первая");
         Line line2 = new Line(2, "Вторая");
         Line line3 = new Line(3, "Третья");
-        addStation("K", line);
+
 
         Station station1 = new Station("Лесная", line);
         Station station2 = new Station("Достоевская", line);
@@ -32,13 +32,13 @@ public class RouteCalculatorTest extends TestCase {
         Station station4 = new Station("Гостиный двор", line);
         Station station5 = new Station("Девяткино", line);
 
-        /* Station station6 = new Station("Гражданский проспект", line2);
+        Station station6 = new Station("Гражданский проспект", line2);
         Station station7 = new Station("Академическая", line2);
         Station station8 = new Station("Политехническая", line2);
         Station station9 = new Station("Площадь Мужества", line2);
-        Station station10 = new Station("Лесная", line2);
+        Station station10 = new Station("Чёрная речка", line2);
 
-        Station station11 = new Station("Выборгская", line3);
+        /* Station station11 = new Station("Выборгская", line3);
         Station station12 = new Station("Площадь Ленина", line3);
         Station station13 = new Station("Чернышевская", line3);
         Station station14 = new Station("Площадь Восстания", line3);
@@ -50,33 +50,35 @@ public class RouteCalculatorTest extends TestCase {
         line.addStation(station4);
         line.addStation(station5);
 
-       /* line2.addStation(station6);
+        line2.addStation(station6);
         line2.addStation(station7);
         line2.addStation(station8);
         line2.addStation(station9);
         line2.addStation(station10);
 
-        line3.addStation(station11);
+       /* line3.addStation(station11);
         line3.addStation(station12);
         line3.addStation(station13);
         line3.addStation(station14);
         line3.addStation(station15);*/
 
         stationIndex.addLine(line);
-       /* stationIndex.addLine(line2);
-        stationIndex.addLine(line3);*/
+        stationIndex.addLine(line2);
+        /* stationIndex.addLine(line3);*/
 
         stationIndex.addStation(station1);
         stationIndex.addStation(station2);
         stationIndex.addStation(station3);
         stationIndex.addStation(station4);
         stationIndex.addStation(station5);
-       /* stationIndex.addStation(station6);
+
+        stationIndex.addStation(station6);
         stationIndex.addStation(station7);
         stationIndex.addStation(station8);
         stationIndex.addStation(station9);
         stationIndex.addStation(station10);
-        stationIndex.addStation(station11);
+
+       /* stationIndex.addStation(station11);
         stationIndex.addStation(station12);
         stationIndex.addStation(station13);
         stationIndex.addStation(station14);
@@ -93,25 +95,45 @@ public class RouteCalculatorTest extends TestCase {
 
     public void testcalculateDuration() throws Exception {
 
-        double expectedTime = 10.0;
+
         Line line = stationIndex.getLine(1);
-        /*Line line2 = stationIndex.getLine(2);
-        Line line3 = stationIndex.getLine(3);*/
+        Line line2 = stationIndex.getLine(2);
+        /* Line line3 = stationIndex.getLine(3);*/
         List<Station> stations = line.getStations();
-       /* List<Station> stations2 = line2.getStations();
-        List<Station> stations3 = line3.getStations(); */
+        List<Station> stations2 = line2.getStations();
+        /* List<Station> stations3 = line3.getStations(); */
         Station station = stations.get(1);
         Station station2 = stations.get(2);
-        /*Station station2 = stations2.get(2);
-        Station station3 = stations3.get(3);*/
-        double actualTime = RouteCalculator.calculateDuration(stations);
+        Station stationLin2 = stations2.get(2);
+        /*Station station3 = stations3.get(3);*/
+
         List<Station> actual = routeCalculator.getShortestRoute(station, station2);
         List<Station> expected = List.of(station, station2);
         assertEquals(expected, actual);
-        assertEquals(expectedTime, actualTime);
+
 
     }
 
+    public void testcalculateDuration2() throws Exception {
+
+        double expectedTime = 10.0;
+        Line line = stationIndex.getLine(1);
+        Line line2 = stationIndex.getLine(2);
+
+        /* Line line3 = stationIndex.getLine(3);*/
+        List<Station> stations = line.getStations();
+        List<Station> stations2 = line2.getStations();
+        /* List<Station> stations3 = line3.getStations(); */
+
+        Station station = stations.get(1);
+
+        Station stationLin2 = stations2.get(2);
+        /*Station station2 = stations2.get(2);
+        Station station3 = stations3.get(3);*/
+        double actualTime = RouteCalculator.calculateDuration(stations2);
+        assertEquals(expectedTime, actualTime);
+
+    }
 
     @Override
     protected void tearDown() throws Exception {
