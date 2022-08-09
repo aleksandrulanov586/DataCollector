a8c175059ce29f3b9982e93fc8d29b5d35332571
Александр Серков <serkov.gcnet@mail.ru>
2022-08-09 16:23:43 +0300

    Реализация логирование

--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/java/Main.java
@@ -2,10 +2,9 @@ import core.Line;
 import core.Station;
 
 
-import org.apache.logging.log4j;
-
-
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
