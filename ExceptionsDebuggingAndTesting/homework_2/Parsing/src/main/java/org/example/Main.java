ae90f45f8bfe0e6296811b2ede1f71bd55b1353b
Александр Серков <serkov.gcnet@mail.ru>
2022-10-18 17:34:28 +0300

    Парсинг станций

---
+++ ExceptionsDebuggingAndTesting/homework_2/Parsing/src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+         Document document = Jsoup.connect("https://skillbox-java.github.io/").get();
+
+        Element all = document.select("body > div > div > div").first();
+        assert all != null;
+        Elements station = all.select("p[class=single-station]");
+       // Elements line= all.select("div[class=js-toggle-depend s-depend-control-single ]");
+        Elements line= all.select("div [class=js-toggle-depend s-depend-control-single  s-depend-control-active] ");
+
+
+        String text = all.text();
+        String text2 = station.text();
+        String text3= line.text();
+System.out.println(text3);
+
+    }
+}
